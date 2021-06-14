6083bcadd5e5f54ecc23d0dc342473d5597939e4
Kohei SUGI <kohei.sg@gmail.com>
2021-06-14 21:45:29 +0900

    Avoid error to parse array JSON

--- test/action_dispatch/test_response/json_test.rb
+++ test/action_dispatch/test_response/json_test.rb
@@ -9,9 +9,15 @@ class ActionDispatch::TestResponse::JsonTest < Minitest::Test
     refute_nil ::ActionDispatch::TestResponse::Json::VERSION
   end
 
-  def test_it_does_something_useful
+  def test_it_does_something_useful_object
     @response.body = {users: [{name: 'yalab', age: 36}]}.to_json
     assert_equal 'yalab', @response.json['users'][0]['name']
     assert_equal 'yalab', @response.json[:users][0][:name]
+  end
+
+  def test_it_does_something_useful_array
+    @response.body = [{name: 'yalab', age: 36}].to_json
+    assert_equal 'yalab', @response.json[0]['name']
+    assert_equal 'yalab', @response.json[0][:name]
   end
 end
